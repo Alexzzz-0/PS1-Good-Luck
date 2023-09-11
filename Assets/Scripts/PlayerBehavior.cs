e72c87e75bd994ce677e7a97ac1ca694b8374824
AlexzzZ-0 <qz2322@nyu.edu>
2023-09-11 13:58:00 -0400

    Still working on it

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -8,8 +8,9 @@ using UnityEngine.UIElements;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    public Vector3 turnAngle = new Vector3(0,100f,0);
-    public float speedLoss = 0.1f;
+    //public Vector3 turnAngle = new Vector3(0,100f,0);
+    public float turnSpeed = 2f;
+    public float runtime = 0.1f;
     
     private Rigidbody rb;
 
@@ -24,14 +25,32 @@ public class PlayerBehavior : MonoBehaviour
         //this.transform.Rotate(Vector3.up * dir * turnSpeed * Time.deltaTime);
         //Debug.Log("Rotata!");
         //rb.DORotate(Vector3.up * dir * turnSpeed * Time.deltaTime, reactSpeed);
-        Quaternion deltaRotation = Quaternion.Euler(turnAngle * Time.deltaTime * dir);
-        rb.MoveRotation(rb.rotation * deltaRotation);
+        // Quaternion deltaRotation = Quaternion.Euler(turnAngle * Time.deltaTime * dir);
+        // rb.MoveRotation(rb.rotation * deltaRotation);
+
+        //Vector3 newRotate = transform.rotation.eulerAngles + turnAngle * Time.deltaTime * dir;
+        transform.Rotate(0,turnSpeed * dir,0);
+        
     }
 
     public void CharacterMoving(float force)
     {
         Debug.Log("move");
-        rb.AddForce( transform.forward * force);
-        
+        //rb.velocity = transform.forward;
+
+
+        Vector3 endPos = transform.position + transform.forward * force;
+        transform.DOMove(endPos, runtime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("stop moving");
+    }
+
+    void StopMoving()
+    {
+        //rb.velocity = Vector3.zero;
+        Debug.Log("stop moving");
     }
 }
