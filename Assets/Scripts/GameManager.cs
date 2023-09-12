d1b562d112a6ae4ad9b06dc9f6ea38cd7d6b0bd3
Alex <zhengqixiaofangfang@gmail.com>
2023-09-11 21:20:07 -0400

    每次他们碰到东西之后会停下

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -36,208 +36,210 @@ public class GameManager : MonoBehaviour
     private bool Crotate = false;
     private bool Drotate = false;
 
-    enum GameState
-    {
-        Start,
-        check,
-        hold,
-        rotate,
-        run
-    }
+//     enum GameState
+//     {
+//         Start,
+//         check,
+//         hold,
+//         rotate,
+//         run
+//     }
+//
+//     private GameState _gameState = GameState.Start;
+//
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.R))
+//         {
+//             int index = SceneManager.GetActiveScene().buildIndex;
+//             SceneManager.LoadScene(index);
+//         }
+//
+//         switch (_gameState)
+//         {
+//             case GameState.Start:
+//                 gameStateText.text = "Start!";
+//                 CRotate();
+//                 if (Input.GetKeyDown(KeyCode.Space))
+//                 {
+//                     _gameState = GameState.run;
+//                 }
+//                 break;
+//             case GameState.check:
+//                 gameStateText.text = "Check!";
+//                 Invoke("check",1f);
+//                 if (Arotate || Brotate || Crotate || Drotate)
+//                 {
+//                     _gameState = GameState.rotate;
+//                 }
+//                 else
+//                 {
+//                     _gameState = GameState.hold;
+//                 }
+//                 break;
+//             case GameState.hold:
+//                 gameStateText.text = "Hold!";
+//                 
+//                 if (Input.GetKeyDown(KeyCode.Space))
+//                 {
+//                     _gameState = GameState.run;
+//                 }
+//                 break;
+//             case GameState.run:
+//                 gameStateText.text = "Move!";
+//                 Move();
+//                 _gameState = GameState.check;
+//                 break;
+//             case GameState.rotate:
+//                 gameStateText.text = "Rotate!";
+//                 if (Arotate)
+//                 {
+//                     ARotate();
+//                 }
+//
+//                 if (Brotate)
+//                 {
+//                     BRotate();
+//                 }
+//                 
+//                 if (Crotate)
+//                 {
+//                     CRotate();
+//                 }
+//                 
+//                 if (Drotate)
+//                 {
+//                     DRotate();
+//                 }
+//
+//                 if (Input.GetKeyDown(KeyCode.Space))
+//                 {
+//                     Move();
+//                 }
+//                 break;
+//         }
+//         
+//         //checkTime -= Time.deltaTime;
+//         //Debug.Log(checkTime.ToString());
+//
+//         // if (Input.GetKeyDown(KeyCode.UpArrow))
+//         // {
+//         //     //canMove = false;
+//         //     humanA.GetComponent<PlayerBehavior>().CharacterMoving(5f);
+//         //     humanB.GetComponent<PlayerBehavior>().CharacterMoving(5f);
+//         //     humanC.GetComponent<PlayerBehavior>().CharacterMoving(1.5f);
+//         //     humanD.GetComponent<PlayerBehavior>().CharacterMoving(2f);
+//         //     checkTime = 3f;
+//         // }
+//     }
+//
+//     void ARotate()
+//     {
+//         if (Input.GetKey(playerAleft))
+//         {
+//             humanA.GetComponent<PlayerBehavior>().RotateTheView(-1);
+//         }else if (Input.GetKey(playerAright))
+//         {
+//             humanA.GetComponent<PlayerBehavior>().RotateTheView(1);
+//         }
+//     }
+//
+//     void BRotate()
+//     {
+//         if (Input.GetKey(playerBleft))
+//         {
+//             humanB.GetComponent<PlayerBehavior>().RotateTheView(-1);
+//         }else if (Input.GetKey(playerBright))
+//         {
+//             humanB.GetComponent<PlayerBehavior>().RotateTheView(1);
+//         }
+//     }
+//
+//     void CRotate()
+//     {
+//         if (Input.GetKey(playerCleft))
+//         {
+//             humanC.GetComponent<PlayerBehavior>().RotateTheView(-1);
+//         }else if (Input.GetKey(playerCright))
+//         {
+//             humanC.GetComponent<PlayerBehavior>().RotateTheView(1);
+//         }
+//     }
+//
+//     void DRotate()
+//     {
+//         if (Input.GetKey(playerDleft))
+//         {
+//             humanD.GetComponent<PlayerBehavior>().RotateTheView(-1);
+//         }else if (Input.GetKey(playerDright))
+//         {
+//             humanD.GetComponent<PlayerBehavior>().RotateTheView(1);
+//         }
+//     }
+//
+//     void check()
+//     {
+//         float AB = Vector3.Distance(humanA.transform.position, humanB.transform.position);
+//         float AC = Vector3.Distance(humanA.transform.position, humanB.transform.position);
+//         float AD = Vector3.Distance(humanA.transform.position, humanB.transform.position);
+//         float BC = Vector3.Distance(humanB.transform.position, humanC.transform.position);
+//         float BD = Vector3.Distance(humanB.transform.position, humanD.transform.position);
+//         float CD = Vector3.Distance(humanC.transform.position, humanD.transform.position);
+//
+//         if (AB <= distance)
+//         {
+//             Debug.Log("AB");
+//             Arotate = true;
+//             Brotate = true;
+//         }
+//         
+//         if (AC <= distance)
+//         {
+//             Arotate = true;
+//             Crotate = true;
+//         }
+//         
+//         if (AD <= distance)
+//         {
+//             Arotate = true;
+//             Drotate = true;
+//         }
+//         
+//         if (BC <= distance)
+//         {
+//             Crotate = true;
+//             Brotate = true;
+//         }
+//         
+//         if (BD <= distance)
+//         {
+//             Drotate = true;
+//             Brotate = true;
+//         }
+//         
+//         if (CD <= distance)
+//         {
+//             Crotate = true;
+//             Drotate = true;
+//         }
+//
+//         //Debug.Log(AB.ToString()+"\n"+AC.ToString()+"\n"+AD.ToString()+"\n"+BC.ToString()+"\n"+BD.ToString()+"\n"+CD.ToString());
+//     }
+//     
+//     void Move()
+//     {
+//         Arotate = false;
+//         Brotate = false;
+//         Crotate = false;
+//         Drotate = false;
+//         
+//         humanA.GetComponent<PlayerBehavior>().CharacterMoving(7f);
+//         humanB.GetComponent<PlayerBehavior>().CharacterMoving(7f);
+//         humanC.GetComponent<PlayerBehavior>().CharacterMoving(7f);
+//         humanD.GetComponent<PlayerBehavior>().CharacterMoving(7f);
+//         
+//     }
 
-    private GameState _gameState = GameState.Start;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            int index = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(index);
-        }
-
-        switch (_gameState)
-        {
-            case GameState.Start:
-                gameStateText.text = "Start!";
-                CRotate();
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    _gameState = GameState.run;
-                }
-                break;
-            case GameState.check:
-                gameStateText.text = "Check!";
-                Invoke("check",1f);
-                if (Arotate || Brotate || Crotate || Drotate)
-                {
-                    _gameState = GameState.rotate;
-                }
-                else
-                {
-                    _gameState = GameState.hold;
-                }
-                break;
-            case GameState.hold:
-                gameStateText.text = "Hold!";
-                
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    _gameState = GameState.run;
-                }
-                break;
-            case GameState.run:
-                gameStateText.text = "Move!";
-                Move();
-                _gameState = GameState.check;
-                break;
-            case GameState.rotate:
-                gameStateText.text = "Rotate!";
-                if (Arotate)
-                {
-                    ARotate();
-                }
-
-                if (Brotate)
-                {
-                    BRotate();
-                }
-                
-                if (Crotate)
-                {
-                    CRotate();
-                }
-                
-                if (Drotate)
-                {
-                    DRotate();
-                }
-
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    Move();
-                }
-                break;
-        }
-        
-        //checkTime -= Time.deltaTime;
-        //Debug.Log(checkTime.ToString());
-
-        // if (Input.GetKeyDown(KeyCode.UpArrow))
-        // {
-        //     //canMove = false;
-        //     humanA.GetComponent<PlayerBehavior>().CharacterMoving(5f);
-        //     humanB.GetComponent<PlayerBehavior>().CharacterMoving(5f);
-        //     humanC.GetComponent<PlayerBehavior>().CharacterMoving(1.5f);
-        //     humanD.GetComponent<PlayerBehavior>().CharacterMoving(2f);
-        //     checkTime = 3f;
-        // }
-    }
-
-    void ARotate()
-    {
-        if (Input.GetKey(playerAleft))
-        {
-            humanA.GetComponent<PlayerBehavior>().RotateTheView(-1);
-        }else if (Input.GetKey(playerAright))
-        {
-            humanA.GetComponent<PlayerBehavior>().RotateTheView(1);
-        }
-    }
-
-    void BRotate()
-    {
-        if (Input.GetKey(playerBleft))
-        {
-            humanB.GetComponent<PlayerBehavior>().RotateTheView(-1);
-        }else if (Input.GetKey(playerBright))
-        {
-            humanB.GetComponent<PlayerBehavior>().RotateTheView(1);
-        }
-    }
-
-    void CRotate()
-    {
-        if (Input.GetKey(playerCleft))
-        {
-            humanC.GetComponent<PlayerBehavior>().RotateTheView(-1);
-        }else if (Input.GetKey(playerCright))
-        {
-            humanC.GetComponent<PlayerBehavior>().RotateTheView(1);
-        }
-    }
-
-    void DRotate()
-    {
-        if (Input.GetKey(playerDleft))
-        {
-            humanD.GetComponent<PlayerBehavior>().RotateTheView(-1);
-        }else if (Input.GetKey(playerDright))
-        {
-            humanD.GetComponent<PlayerBehavior>().RotateTheView(1);
-        }
-    }
-
-    void check()
-    {
-        float AB = Vector3.Distance(humanA.transform.position, humanB.transform.position);
-        float AC = Vector3.Distance(humanA.transform.position, humanB.transform.position);
-        float AD = Vector3.Distance(humanA.transform.position, humanB.transform.position);
-        float BC = Vector3.Distance(humanB.transform.position, humanC.transform.position);
-        float BD = Vector3.Distance(humanB.transform.position, humanD.transform.position);
-        float CD = Vector3.Distance(humanC.transform.position, humanD.transform.position);
-
-        if (AB <= distance)
-        {
-            Debug.Log("AB");
-            Arotate = true;
-            Brotate = true;
-        }
-        
-        if (AC <= distance)
-        {
-            Arotate = true;
-            Crotate = true;
-        }
-        
-        if (AD <= distance)
-        {
-            Arotate = true;
-            Drotate = true;
-        }
-        
-        if (BC <= distance)
-        {
-            Crotate = true;
-            Brotate = true;
-        }
-        
-        if (BD <= distance)
-        {
-            Drotate = true;
-            Brotate = true;
-        }
-        
-        if (CD <= distance)
-        {
-            Crotate = true;
-            Drotate = true;
-        }
-
-        //Debug.Log(AB.ToString()+"\n"+AC.ToString()+"\n"+AD.ToString()+"\n"+BC.ToString()+"\n"+BD.ToString()+"\n"+CD.ToString());
-    }
     
-    void Move()
-    {
-        Arotate = false;
-        Brotate = false;
-        Crotate = false;
-        Drotate = false;
-        
-        humanA.GetComponent<PlayerBehavior>().CharacterMoving(7f);
-        humanB.GetComponent<PlayerBehavior>().CharacterMoving(7f);
-        humanC.GetComponent<PlayerBehavior>().CharacterMoving(7f);
-        humanD.GetComponent<PlayerBehavior>().CharacterMoving(7f);
-        
-    }
 }
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using TMPro;
+using Unity.VisualScripting;
 using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -9,48 +11,132 @@ using UnityEngine.UIElements;
 public class PlayerBehavior : MonoBehaviour
 {
     //public Vector3 turnAngle = new Vector3(0,100f,0);
+    //[SerializeField] private TextMeshPro objectVelocity;
+
+    public KeyCode left;
+    public KeyCode right;
+    
     public float turnSpeed = 2f;
     public float runtime = 0.1f;
-    
-    private Rigidbody rb;
+
+    public float quickTurnSpeed = 2f;
+    public float slowTurnSpeed = 2f;
+    public float moveSpeed = 10f;
+    public float velocity;
 
     private void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        //rb = this.GetComponent<Rigidbody>();
     }
 
-    public void RotateTheView(int dir)
+    enum ObjectState
     {
-        
-        //this.transform.Rotate(Vector3.up * dir * turnSpeed * Time.deltaTime);
-        //Debug.Log("Rotata!");
-        //rb.DORotate(Vector3.up * dir * turnSpeed * Time.deltaTime, reactSpeed);
-        // Quaternion deltaRotation = Quaternion.Euler(turnAngle * Time.deltaTime * dir);
-        // rb.MoveRotation(rb.rotation * deltaRotation);
-
-        //Vector3 newRotate = transform.rotation.eulerAngles + turnAngle * Time.deltaTime * dir;
-        transform.Rotate(0,turnSpeed * dir,0);
-        
+        rotate,
+        move
     }
 
-    public void CharacterMoving(float force)
+    private ObjectState _objectState = ObjectState.rotate;
+    
+    public void Update()
     {
-        Debug.Log("move");
-        //rb.velocity = transform.forward;
+        //objectVelocity.text = velocity.ToString();
 
+        switch (_objectState)
+        {
+            case ObjectState.rotate:
+                if (Input.GetKey(left))
+                {
+                    transform.Rotate(0,quickTurnSpeed * (-1),0);
+                }else if (Input.GetKey(right))
+                {
+                    transform.Rotate(0,quickTurnSpeed,0);
+                }
+                
+                if (Input.GetKeyUp(left) || Input.GetKeyUp(right))
+                {
+                    _objectState = ObjectState.move;
+                }
+                break;
+            case ObjectState.move:
 
-        Vector3 endPos = transform.position + transform.forward * force;
-        transform.DOMove(endPos, runtime);
+                // if (velocity >= 0)
+                // {
+                //     velocity -= Time.deltaTime;
+                //     
+                //     
+                //    
+                //
+                //     // if (Input.GetKey(left))
+                //     // {
+                //     //     transform.Rotate(0,slowTurnSpeed * Time.deltaTime * (-1),0);
+                //     // }else if (Input.GetKey(right))
+                //     // {
+                //     //     transform.Rotate(0,slowTurnSpeed * Time.deltaTime,0);
+                //     // }
+                // }
+                
+                Vector3 newPos = transform.position + transform.forward * moveSpeed * Time.deltaTime;
+                transform.position = newPos;
+                break;
+        }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log("stop moving");
-    }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("crush");
+            _objectState = ObjectState.rotate;
+            velocity = (other.gameObject.GetComponent<PlayerBehavior>().velocity + velocity) / 2;
+        }
 
-    void StopMoving()
-    {
-        //rb.velocity = Vector3.zero;
-        Debug.Log("stop moving");
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("Wall");
+            _objectState = ObjectState.rotate;
+        }
+
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("done");
+            Destroy(gameObject);
+        }
     }
+    
+    
+    
+    // public void RotateTheView(int dir)
+    // {
+    //     
+    //     //this.transform.Rotate(Vector3.up * dir * turnSpeed * Time.deltaTime);
+    //     //Debug.Log("Rotata!");
+    //     //rb.DORotate(Vector3.up * dir * turnSpeed * Time.deltaTime, reactSpeed);
+    //     // Quaternion deltaRotation = Quaternion.Euler(turnAngle * Time.deltaTime * dir);
+    //     // rb.MoveRotation(rb.rotation * deltaRotation);
+    //
+    //     //Vector3 newRotate = transform.rotation.eulerAngles + turnAngle * Time.deltaTime * dir;
+    //     transform.Rotate(0,turnSpeed * dir,0);
+    //     
+    // }
+    //
+    // public void CharacterMoving(float force)
+    // {
+    //     Debug.Log("move");
+    //     //rb.velocity = transform.forward;
+    //
+    //
+    //     Vector3 endPos = transform.position + transform.forward * force;
+    //     transform.DOMove(endPos, runtime);
+    // }
+    //
+    // private void OnCollisionEnter(Collision other)
+    // {
+    //     Debug.Log("stop moving");
+    // }
+    //
+    // void StopMoving()
+    // {
+    //     //rb.velocity = Vector3.zero;
+    //     Debug.Log("stop moving");
+    // }
 }
